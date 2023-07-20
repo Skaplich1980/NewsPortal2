b03d682579394a98c4fc036c7cc5234ce2325256
Dzed Baradzed <skaplich@mail.ru>
2023-07-20 19:48:45 +0300

    2

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -19,4 +19,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
+    # подключались к главному приложению с префиксом news/.
+    path('news/', include('simpleapp.urls')),
+
 ]
